b7d550665b43e808b6110f715ef95c148a1ed798
Tizicla <mon.glez98@gmail.com>
2024-10-14 09:12:43 -0600

    Auth for routes

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -1,19 +1,22 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe, UseInterceptors, UploadedFile } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
-
 import { EmployeesService } from './employees.service';
 import { CreateEmployeeDto } from './dto/create-employee.dto';
 import { UpdateEmployeeDto } from './dto/update-employee.dto';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { ROLES } from 'src/auth/constants/roles.constants';
 
 @Controller('employees')
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
 
+  @Auth(ROLES.MANAGER)
   @Post()
   create(@Body() createEmployeeDto: CreateEmployeeDto) {
     return this.employeesService.create(createEmployeeDto);
   }
 
+  @Auth(ROLES.EMPLOYEE, ROLES.MANAGER)
   @Post('upload')
   @UseInterceptors(FileInterceptor('file'))
   uploadPhoto(@UploadedFile() file: Express.Multer.File){
@@ -22,12 +25,13 @@ export class EmployeesController {
 
 
 
-
+  @Auth(ROLES.MANAGER)
   @Get()
   findAll() {
     return this.employeesService.findAll();
   }
 
+  @Auth(ROLES.MANAGER)
   @Get('/:id') // Decorador que indica que se va a recibir un parámetro en la ruta.
   findOne(
     @Param('id', new ParseUUIDPipe({version: '4'})) // Decorador que modifica el valor de id para que absorba el valor de la ruta.
@@ -36,11 +40,13 @@ export class EmployeesController {
     return this.employeesService.findOne(id); // Se llama al método findOne del servicio y se le pasa el id. 
   }
 
+  @Auth(ROLES.EMPLOYEE)
   @Patch('/:id')
   update(@Param('id', new ParseUUIDPipe({version: '4'})) id: string, @Body() updateEmployeeDto: UpdateEmployeeDto) {
     return this.employeesService.update(id, updateEmployeeDto);
   }
 
+  @Auth(ROLES.MANAGER)
   @Delete('/:id')
   remove(
   @Param('id', new ParseUUIDPipe({version: '4'})) 
